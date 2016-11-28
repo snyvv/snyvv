93adb3d1f614c1ae6c94b87e4ef5f6bde68f500d
snyvv88 <snyvv88@gmail.com>
2016-11-28 20:30:14 +0900

    routing

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'snyvv' });
+});
+
+/* about */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'ABOUT | snyvv' });
+});
+
+/* portfolio */
+router.get('/portfolio', function(req, res, next) {
+  res.render('portfolio', { title: 'PORTFOLIO | snyvv' });
+});
+
+/* contact */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'CONTACT | snyvv' });
 });
 
 module.exports = router;
