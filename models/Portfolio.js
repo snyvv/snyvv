4f90ffcd6ea39923cb21b4ac02b81ed94b4131c9
amityaffliction <convalescence21@gmail.com>
2016-12-12 21:48:21 +0900

    search

--- models/Portfolio.js
+++ models/Portfolio.js
@@ -18,27 +18,10 @@ module.exports = {
 		return this.hasMany('Comment');
 	},
 	methods:{
-
 		search: function(keyword){
-
-	  	let models = require('./');
-
-	  	return models.transaction(t => {
-
-	  			return models.Portfolio.query((q) => {
-						  q.where('name', 'LIKE', '%'+keyword+'%')
-						  .orWhere('subname', 'LIKE', '%'+keyword+'%')
-						  .orWhere('contents', 'LIKE', '%'+keyword+'%');
-						})
-						.fetchAll({withRelated:['tags'] ,transacting:t});
-
-	  	});
-	  },
-		searchAll: function(keyword){
-
 	  	let models = require('./');
 	  	return models.query(
-	  		`select * from portfolios 
+	  		`select * from portfolios
 	  		where name like ? 
 	  		or subname like ? 
 	  		or contents like ?
@@ -48,11 +31,48 @@ module.exports = {
 	  		`%${keyword}%`,
 	  		`%${keyword}%`]);
 	  },
-		searchFind: function(keyword){
-
+	  searchAll: function(keyword){
 	  	let models = require('./');
-
-	  	return models.query('select * from portfolios');
+	  	return Promise.all([
+	  		this.search(keyword),
+	  		models.query(
+	  		`
+	  		SELECT portfolios.id,portfolios.name,portfolios.subname,portfolios.image, portfolios.contents, portfolios.date, tags.name as tag_name, tags.eng as tag_eng
+			FROM (portfolios join portfolios_tags on portfolios.id = portfolios_tags.portfolio_id) join tags on portfolios_tags.tag_id = tags.id
+	  		where portfolios.name like ? 
+	  		or portfolios.subname like ? 
+	  		or portfolios.contents like ?
+	  		or DATE_FORMAT(portfolios.date, '%Y-%m-%d') LIKE ?
+	  		or tags.name like ?
+	  		or tags.eng like ?
+	  		`
+	  		,[`%${keyword}%`,
+	  		`%${keyword}%`,
+	  		`%${keyword}%`,
+	  		`%${keyword}%`,
+	  		`%${keyword}%`,
+	  		`%${keyword}%`,])	  		
+	  		]) 
+	  	.then(result=>{
+	  		var data = result[0][0].concat(result[1][0]);
+	  		var ids = [];
+	  		data.map(d=>{
+	  			ids.push(d.id);
+	  		});
+	  		uniqueIds = ids.filter(function(item, pos) {
+			    return ids.indexOf(item) == pos;
+			});
+	  		return Promise.resolve(uniqueIds);
+	  	})
+	  	.then(result =>{
+	  		return models.Portfolio.forge()
+	  				 .where('id', 'in', result)
+	  				 .fetchAll({withRelated:['tags']});
+	  	})
+	  	.catch(err=>{
+	  		console.log(err);
+	  	});
+	  	;
 	  },
 	}
 }
